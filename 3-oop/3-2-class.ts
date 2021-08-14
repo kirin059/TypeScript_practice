62a23ef161d53356a69a9890bc282f490a083e2c
saebom <boma91@gmail.com>
2021-08-14 19:21:03 +0900

    Add: oop 내용 추가 완료

--- 3-oop/3-2-class.ts
+++ 3-oop/3-2-class.ts
@@ -6,13 +6,14 @@
 
     class CoffeeMaker {
         // 멤버변수(class내에서는 멤버변수에 const 또는 let을 붙이지 않는다, 함수도 마찬가지로 function을 따로 붙이지 않는다)
-        static BEANS_GRAMM_PER_SHOT: number = 7  // class level(static을 붙인경우)
-        coffeeBeans: number = 0;                 // instance (object) level(static을 안 붙인경우)
+        static BEANS_GRAMM_PER_SHOT: number = 7  // class level  (static을 붙인경우)
+        coffeeBeans: number = 0;                 // instance (object) level  (static을 안 붙인경우)
 
         constructor(beans: number) {
             this.coffeeBeans = beans;
         }
 
+        // makeMachine 함수에 static을 붙였기 때문에 인스턴스에서 사용할 수 없다. 해당 함수를 외부에서 호출하고 싶다면 "변수 maker2" 처럼 가능
         static makeMachine(coffeeBeens: number): CoffeeMaker {
             return new CoffeeMaker(coffeeBeens)
         }
@@ -31,6 +32,5 @@
     }
 
     const maker = new CoffeeMaker(32)  // maker안에는 커피콩이 32개 전달된다
-    const maker2 = CoffeeMaker.makeMachine(3)
-    const maker3 = CoffeeMaker.makeMachine(5)
+    const maker2 = CoffeeMaker.makeMachine(3)   // 생성자 함수를 통해 호출하는 것이 아니라 "클래스에서 직접 함수 호출"
 }
