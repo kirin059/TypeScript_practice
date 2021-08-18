9e4083d7c691ddab1cf500a98d5b270ed3717d7d
saebom <boma91@gmail.com>
2021-08-18 18:15:11 +0900

    Add: interface

--- 3-oop/3-4-abstraction.ts
+++ 3-oop/3-4-abstraction.ts
@@ -4,7 +4,12 @@
         hasMilk: boolean;
     };
 
-    class CoffeeMaker {
+    // 외부적으로 사용하는 이름. 즉, 해당 Interface를 사용하여 외부에서 접근가능한 함수만 노출시킬 수 있다.
+    interface CoffeeMaker  {
+        makeCoffee(shots: number): CoffeeCup;
+    }
+
+    class CoffeeMachine implements CoffeeMaker {
 
         private static BEANS_GRAMM_PER_SHOT: number = 7
         private coffeeBeans: number = 0;
@@ -13,8 +18,8 @@
             this.coffeeBeans = beans;
         }
 
-        static makeMachine(coffeeBeens: number): CoffeeMaker {
-            return new CoffeeMaker(coffeeBeens)
+        static makeMachine(coffeeBeens: number): CoffeeMachine {
+            return new CoffeeMachine(coffeeBeens)
         }
 
         fillCoffeeBeans(beans: number) {
@@ -27,10 +32,10 @@
 
 
         private grindBeans(shots:number) {
-            if (this.coffeeBeans < shots * CoffeeMaker.BEANS_GRAMM_PER_SHOT) {
+            if (this.coffeeBeans < shots * CoffeeMachine.BEANS_GRAMM_PER_SHOT) {
                 throw new Error('Not enough coffee beens!');
             }
-            this.coffeeBeans -= shots * CoffeeMaker.BEANS_GRAMM_PER_SHOT;
+            this.coffeeBeans -= shots * CoffeeMachine.BEANS_GRAMM_PER_SHOT;
         }
         private preheat(): void {
             console.log('heating up...')
@@ -49,15 +54,9 @@
 
     }
 
-    const maker = CoffeeMaker.makeMachine(32)  
-    // maker.fillCoffeeBeans(32)
-    // maker.extract(23)
-    // maker.grindBeans(12)
-    // maker.preheat
-    // 위처럼 커피를 만들기 위해서는 여러개의 함수를 호출해야한다. 인터페이스가 너무 장황해지므로 
-    // 간략하게 makeCoffee함수만 호출해서 커피를 만들고 싶다면 "정보은닉"을 통해 간단화 할 수 있다 (private붙여주기)
+    const maker: CoffeeMaker = CoffeeMachine.makeMachine(32)  
 
-    maker.fillCoffeeBeans(32)
+    maker.fillCoffeeBeans(32)  // interface에 없기때문에 호출안됨 (maker의 타입이 인터페이스(=CoffeeMaker)이기 때문)
     maker.makeCoffee(2)
     // 정말 필요한 함수만 노출가능
 }
